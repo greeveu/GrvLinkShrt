0ae3defb49812a633a9968a33b140e895bbf93ff
Alexander <alex@pluoi.com>
2023-02-14 23:13:05 +0100

    extract functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::str;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 use serde::{Deserialize, Serialize};
 use worker::wasm_bindgen::JsValue;
@@ -11,155 +8,166 @@ use worker::*;
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let router = Router::new();
-    let decoding_key = DecodingKey::from_rsa_pem(public_key).unwrap();
 
     router
         .get("/", |_, _| build_response("OK", 200))
-        .get_async("/redirect/:key", |_, ctx| async move {
-            if let Some(key) = ctx.param("key") {
-                let db: D1Database = ctx.env.d1("DB")?;
-
-                let statement = db
-                    .prepare("SELECT long_url FROM grv_links WHERE key =?")
-                    .bind(&[JsValue::from(key)])?;
-
-                let results: Option<String> = statement.first(Some("long_url")).await?;
-
-                return match results {
-                    None => {
-                        Ok(build_response("URL Unknown", 404)?.with_headers(build_headers()?))
-                    }
-                    Some(url) => {
-                        let d1result = increment_link_clicks(key, &db).await?;
-                        if !d1result.success() {
-                            console_log!("Error while updating clicks for link {key:}")
-                        }
-                        Response::redirect(Url::parse(url.as_str())?)
-                    }
-                };
-            }
-            Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
-        })
-        .get_async("/list", |req, ctx| async move {
-            let db: D1Database = ctx.env.d1("DB")?;
-            let hash_query: HashMap<_, _> = req.url().unwrap().query_pairs().into_owned().collect();
-
-            let statement = if hash_query.get("show_unlisted").unwrap_or(&"false".to_string()).eq("true") {
-                if let Some(err) = check_apikey(&req, &ctx) {
-                    return err;
-                }
-                db.prepare("SELECT * FROM grv_links")
-            } else {
-                db.prepare("SELECT * FROM grv_links WHERE unlisted = 0")
-            };
-
-
-            let d1_results = statement.all().await?;
-            let link_results = d1_results.results::<Link>()?;
-
-            Ok(build_response(serde_json::to_string(link_results.as_slice())?.as_str(), 200)?.with_headers(build_headers()?))
-        })
-        .get_async("/info/:key", |_, ctx| async move {
-            if let Some(key) = ctx.param("key") {
-                let db: D1Database = ctx.env.d1("DB")?;
-
-                let statement = db
-                    .prepare("SELECT * FROM grv_links WHERE key = ?")
-                    .bind(&[JsValue::from(key)])?;
-
-                let results = statement.all().await?;
-
-                let result = results.results::<Link>()?;
-
-                return match result.get(0) {
-                    None => return Ok(build_response("Key not found", 404)?.with_headers(build_headers()?)),
-                    Some(url) => Ok(build_response(serde_json::to_string(url)?.as_str(), 200)?.with_headers(build_headers()?)),
-                };
-            }
-            Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
-        })
-        .put_async("/", |mut req, ctx| async move {
-            if let Some(err) = check_apikey(&req, &ctx) {
-                return err;
-            }
-
-            let buf = req.bytes().await?;
-
-            if let Ok(data) = str::from_utf8(buf.as_slice()) {
-                let result: Link = serde_json::from_str(data)?;
-
-                let db: D1Database = ctx.env.d1("DB")?;
-
-                let statement = db
-                    .prepare("INSERT INTO grv_links(`key`, `long_url`, `clicks`, `unlisted`) VALUES (?,?,?,?)")
-                    .bind(&[
-                        JsValue::from(result.key.as_str()),
-                        JsValue::from(result.long_url.as_str()),
-                        JsValue::from(result.clicks.to_string()),
-                        JsValue::from(result.unlisted.unwrap())])?;
-
-                return match statement.run().await {
-                    Ok(_) => Ok(build_response("OK", 200)?.with_headers(build_headers()?)),
-                    Err(_) => Ok(build_response("Database error!", 500)?.with_headers(build_headers()?))
-                };
-            }
-
-            Ok(build_response("Invalid data send", 500)?.with_headers(build_headers()?))
-        })
-        .patch_async("/", |mut req, ctx| async move {
-            if let Some(err) = check_apikey(&req, &ctx) {
-                return err;
-            }
-
-            let buf = req.bytes().await?;
-
-            if let Ok(data) = str::from_utf8(buf.as_slice()) {
-                let result: Link = serde_json::from_str(data)?;
-
-                let db: D1Database = ctx.env.d1("DB")?;
-
-                let statement = db
-                    .prepare("INSERT INTO grv_links(`key`, `long_url`, `clicks`, `unlisted`) VALUES (?,?,?,?) ON CONFLICT(`key`) DO UPDATE SET `long_url` = ?, `clicks` = ?, `unlisted` = ?")
-                    .bind(&[
-                        JsValue::from(result.key.as_str()),
-                        JsValue::from(result.long_url.as_str()),
-                        JsValue::from(result.clicks.to_string()),
-                        JsValue::from(result.unlisted.unwrap()),
-                        JsValue::from(result.long_url.as_str()),
-                        JsValue::from(result.clicks.to_string()),
-                        JsValue::from(result.unlisted.unwrap())])?;
-
-                return match statement.run().await {
-                    Ok(_) => Ok(build_response("OK", 200)?.with_headers(build_headers()?)),
-                    Err(_) => Ok(build_response("Database error!", 500)?.with_headers(build_headers()?))
-                };
-            }
-
-            Ok(build_response("Invalid data send", 500)?.with_headers(build_headers()?))
-        })
-        .delete_async("/:key", |req, ctx| async move {
-            if let Some(err) = check_apikey(&req, &ctx) {
-                return err;
-            }
-
-            if let Some(key) = ctx.param("key") {
-                let db: D1Database = ctx.env.d1("DB")?;
-
-                let statement = db
-                    .prepare("DELETE FROM grv_links WHERE key = ? ")
-                    .bind(&[JsValue::from(key)])?;
-
-                let results = statement.run().await?;
-                return if results.success() {
-                    Ok(build_response("Link deleted", 200)?.with_headers(build_headers()?))
-                } else {
-                    Ok(build_response("Can not delete link", 500)?.with_headers(build_headers()?))
-                };
-            }
-            Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
-        })
+        .get_async("/redirect/:key", redirect)
+        .get_async("/list", list_links)
+        .get_async("/info/:key", link_info)
+        .put_async("/", add_link)
+        .patch_async("/", patch_link)
+        .delete_async("/:key", delete_link)
         .run(req, env)
         .await
+}
+
+async fn redirect(_: Request, ctx: RouteContext<()>) -> Result<Response> {
+    if let Some(key) = ctx.param("key") {
+        let db: D1Database = ctx.env.d1("DB")?;
+
+        let statement = db
+            .prepare("SELECT long_url FROM grv_links WHERE key =?")
+            .bind(&[JsValue::from(key)])?;
+
+        let results: Option<String> = statement.first(Some("long_url")).await?;
+
+        return match results {
+            None => {
+                Ok(build_response("URL Unknown", 404)?.with_headers(build_headers()?))
+            }
+            Some(url) => {
+                let d1result = increment_link_clicks(key, &db).await?;
+                if !d1result.success() {
+                    console_log!("Error while updating clicks for link {key:}")
+                }
+                Response::redirect(Url::parse(url.as_str())?)
+            }
+        };
+    }
+    Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
+}
+
+async fn list_links(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let db: D1Database = ctx.env.d1("DB")?;
+    let hash_query: HashMap<_, _> = req.url().unwrap().query_pairs().into_owned().collect();
+
+    let statement = if hash_query.get("show_unlisted").unwrap_or(&"false".to_string()).eq("true") {
+        if let Some(err) = check_apikey(&req, &ctx) {
+            return err;
+        }
+        db.prepare("SELECT * FROM grv_links")
+    } else {
+        db.prepare("SELECT * FROM grv_links WHERE unlisted = 0")
+    };
+
+
+    let d1_results = statement.all().await?;
+    let link_results = d1_results.results::<Link>()?;
+
+    Ok(build_response(serde_json::to_string(link_results.as_slice())?.as_str(), 200)?.with_headers(build_headers()?))
+}
+
+async fn link_info(_: Request, ctx: RouteContext<()>) -> Result<Response> {
+    if let Some(key) = ctx.param("key") {
+        let db: D1Database = ctx.env.d1("DB")?;
+
+        let statement = db
+            .prepare("SELECT * FROM grv_links WHERE key = ?")
+            .bind(&[JsValue::from(key)])?;
+
+        let results = statement.all().await?;
+
+        let result = results.results::<Link>()?;
+
+        return match result.get(0) {
+            None => return Ok(build_response("Key not found", 404)?.with_headers(build_headers()?)),
+            Some(url) => Ok(build_response(serde_json::to_string(url)?.as_str(), 200)?.with_headers(build_headers()?)),
+        };
+    }
+    Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
+}
+
+async fn add_link(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    if let Some(err) = check_apikey(&req, &ctx) {
+        return err;
+    }
+
+    let buf = req.bytes().await?;
+
+    if let Ok(data) = str::from_utf8(buf.as_slice()) {
+        let result: Link = serde_json::from_str(data)?;
+
+        let db: D1Database = ctx.env.d1("DB")?;
+
+        let statement = db
+            .prepare("INSERT INTO grv_links(`key`, `long_url`, `clicks`, `unlisted`) VALUES (?,?,?,?)")
+            .bind(&[
+                JsValue::from(result.key.as_str()),
+                JsValue::from(result.long_url.as_str()),
+                JsValue::from(result.clicks.to_string()),
+                JsValue::from(result.unlisted.unwrap())])?;
+
+        return match statement.run().await {
+            Ok(_) => Ok(build_response("OK", 200)?.with_headers(build_headers()?)),
+            Err(_) => Ok(build_response("Database error!", 500)?.with_headers(build_headers()?))
+        };
+    }
+
+    Ok(build_response("Invalid data send", 500)?.with_headers(build_headers()?))
+}
+
+async fn delete_link(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    if let Some(err) = check_apikey(&req, &ctx) {
+        return err;
+    }
+
+    if let Some(key) = ctx.param("key") {
+        let db: D1Database = ctx.env.d1("DB")?;
+
+        let statement = db
+            .prepare("DELETE FROM grv_links WHERE key = ? ")
+            .bind(&[JsValue::from(key)])?;
+
+        let results = statement.run().await?;
+        return if results.success() {
+            Ok(build_response("Link deleted", 200)?.with_headers(build_headers()?))
+        } else {
+            Ok(build_response("Can not delete link", 500)?.with_headers(build_headers()?))
+        };
+    }
+    Ok(build_response("Key Missing", 400)?.with_headers(build_headers()?))
+}
+
+async fn patch_link(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    if let Some(err) = check_apikey(&req, &ctx) {
+        return err;
+    }
+
+    let buf = req.bytes().await?;
+
+    if let Ok(data) = str::from_utf8(buf.as_slice()) {
+        let result: Link = serde_json::from_str(data)?;
+
+        let db: D1Database = ctx.env.d1("DB")?;
+
+        let statement = db
+            .prepare("INSERT INTO grv_links(`key`, `long_url`, `clicks`, `unlisted`) VALUES (?,?,?,?) ON CONFLICT(`key`) DO UPDATE SET `long_url` = ?, `clicks` = ?, `unlisted` = ?")
+            .bind(&[
+                JsValue::from(result.key.as_str()),
+                JsValue::from(result.long_url.as_str()),
+                JsValue::from(result.clicks.to_string()),
+                JsValue::from(result.unlisted.unwrap()),
+                JsValue::from(result.long_url.as_str()),
+                JsValue::from(result.clicks.to_string()),
+                JsValue::from(result.unlisted.unwrap())])?;
+
+        return match statement.run().await {
+            Ok(_) => Ok(build_response("OK", 200)?.with_headers(build_headers()?)),
+            Err(_) => Ok(build_response("Database error!", 500)?.with_headers(build_headers()?))
+        };
+    }
+
+    Ok(build_response("Invalid data send", 500)?.with_headers(build_headers()?))
 }
 
 fn build_headers() -> Result<Headers> {
@@ -198,38 +206,6 @@ fn check_apikey(req: &Request, ctx: &RouteContext<()>) -> Option<Result<Response
     }
 }
 
-fn check_jwt(req: &Request, ctx: &RouteContext<()>, required_permission: Permission) -> Option<Result<Response>> {
-    let public_key = ctx.var("JWT_PUBLIC_KEY");
-    if public_key.is_err() {
-        return Some(build_response("Unable to authenticate request.", 500));
-    }
-
-    let token = req.headers().get("Bearer");
-    if token.is_err() || token.as_ref().unwrap().is_none() {
-        return Some(build_response("JWT missing", 401));
-    }
-
-    let token_message = decode::<User>(
-        &token.unwrap().unwrap(),
-        &DecodingKey::from_secret("secret".as_ref()),
-        &Validation::new(Algorithm::ES256),
-    );
-    if token_message.is_err() {
-        return Some(build_response("JWT invalid.", 401));
-    }
-
-    let token_data = token_message.unwrap();
-
-    //We can unwrap here since I don't think SystemTime::now() will ever return a time before UNIX_EPOCH.
-    let duration = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    if token_data.claims.valid_until < duration.as_secs() ||
-        !token_data.claims.permission.contains(&required_permission) {
-        return Some(build_response("JWT invalid.", 401));
-    }
-
-    None
-}
-
 async fn increment_link_clicks(key: &String, database: &D1Database) -> Result<D1Result> {
     let statement = database
         .prepare("UPDATE grv_links SET `clicks`=`clicks` + 1 WHERE `key` = ?")
@@ -245,19 +221,4 @@ struct Link {
     clicks: usize,
     #[serde(skip_serializing)]
     unlisted: Option<u8>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    id: String,
-    permission: Vec<Permission>,
-    //Unix Timestamp
-    valid_until: u64,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum Permission {
-    Read,
-    Write,
-    Delete,
 }
